0449a9740ad1a247685b6523f778438b1053f42a
carlosaruy <57971561+carlosaruy@users.noreply.github.com>
2021-09-24 14:23:00 +0000

    templates
    templates - en proceso

--- app.py
+++ app.py
@@ -13,14 +13,21 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     #return "<h1>Bienvenidos!</h1>"
-    return render_template("index.html")
+    #render template busca por defecto en el directorio templates.
+    #return render_template("index.html")
+    #ahora - enviamos variables para que index.html las procese...
+    # mediante jinja
+    return render_template("index.html", una_variable_cualquiera="juan paco pedro de la mar")
 #testear flask:
 #desde terminal ejecutar
 # flask run 
 # automaticamente buscará en el directorio de trabajo al archivo app.py
 @app.route("/chau")
+@app.route("/adios")
+@app.route("/bye")
 def bye():
-    return "Hasta luego!"
+    #return "Hasta luego!"
+    return render_template("test2.html")
 
 if __name__ == "__main__":
     #pass
