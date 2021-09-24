2f5f3b2f5c6e7f6222c96e8c269a801b928e0a86
carlosaruy <57971561+carlosaruy@users.noreply.github.com>
2021-09-24 15:46:35 +0000

    sql alchemy agregado
    agrego sqlalchemy - y creacion de base de datos con sqlite.

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 #para no cargar todo el html en el codigo python
 #se utiliza el render_template.
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"]="UNA_PASSWORD_SUPERSEGURA_EN_GITHUB"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' 
+
+db = SQLAlchemy(app)
+
+
 from routes import * 
 
 if __name__ == "__main__":
